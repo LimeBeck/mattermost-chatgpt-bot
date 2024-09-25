5fe72f64098c8aa48b840edf9e30b360189f96cc
Anatoly Nechay-Gumen <mail@limebeck.dev>
2024-09-25 13:35:53 +0000

    Fix

--- src/main/kotlin/mattermost/MattermostClientImpl.kt
+++ src/main/kotlin/mattermost/MattermostClientImpl.kt
@@ -14,8 +14,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
